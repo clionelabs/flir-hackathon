e3c3ac3a42b6eba173c107e122505274373901f8
Wat Chun Pang Gilbert <gilbert.wat@getthingsdone.hk>
2015-11-22 07:08:53 +0800

    Added a lot of things

--- app/src/main/java/com/clionelabs/flirhackathon/MainActivity.java
+++ app/src/main/java/com/clionelabs/flirhackathon/MainActivity.java
@@ -1,13 +1,514 @@
 package com.clionelabs.flirhackathon;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.location.Location;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.content.Context;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.OrientationEventListener;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+import com.firebase.client.Firebase;
+import com.flir.flironesdk.Device;
+import com.flir.flironesdk.Frame;
+import com.flir.flironesdk.FrameProcessor;
+import com.flir.flironesdk.RenderedImage;
+import com.flir.flironesdk.LoadedFrame;
+import com.flir.flironesdk.SimulatedDevice;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.ByteBuffer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.EnumSet;
+import java.util.Locale;
+
+/**
+ * An example activity and delegate for FLIR One image streaming and device interaction.
+ * Based on an example full-screen activity that shows and hides the system UI (i.e.
+ * status bar and navigation/system bar) with user interaction.
+ *
+ * @see com.flir.flironesdk.Device.Delegate
+ * @see com.flir.flironesdk.FrameProcessor.Delegate
+ * @see com.flir.flironesdk.Device.StreamDelegate
+ * @see com.flir.flironesdk.Device.PowerUpdateDelegate
+ */
+public class MainActivity extends Activity implements
+        Device.Delegate,
+        FrameProcessor.Delegate,
+        Device.StreamDelegate,
+        Device.PowerUpdateDelegate,
+        GoogleApiClient.ConnectionCallbacks, LocationListener,
+        GoogleApiClient.OnConnectionFailedListener
+
+{
+    ImageView thermalImageView;
+    private volatile boolean imageCaptureRequested = false;
+    private boolean chargeCableIsConnected = true;
+
+    private volatile Device flirOneDevice;
+    private FrameProcessor frameProcessor;
+
+    private String lastSavedPath;
+
+    private Device.TuningState currentTuningState = Device.TuningState.Unknown;
+
+    private TextView tvInfo;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private LocationRequest mLocationRequest;
+    private Boolean mRequestingLocationUpdates = Boolean.TRUE;
+    private String REQUESTING_LOCATION_UPDATES_KEY = "REQUESTING_LOCATION_UPDATES_KEY";
+    private String LOCATION_KEY = "LOCATION_KEY";
+    private String LAST_UPDATED_TIME_STRING_KEY = "LAST_UPDATED_TIME_STRING_KEY";
+    private Runnable timer;
+    private Handler handler;
+    // Device Delegate methods
+
+    // Called during device discovery, when a device is connected
+    // During this callback, you should save a reference to device
+    // You should also set the power update delegate for the device if you have one
+    // Go ahead and start frame stream as soon as connected, in this use case
+    // Finally we create a frame processor for rendering frames
+
+    public void onDeviceConnected(Device device){
+        Log.i("ExampleApp", "Device connected!");
+
+        flirOneDevice = device;
+        flirOneDevice.setPowerUpdateDelegate(this);
+        flirOneDevice.startFrameStream(this);
+
+        final ToggleButton chargeCableButton = (ToggleButton)findViewById(R.id.chargeCableToggle);
+        if(flirOneDevice instanceof SimulatedDevice){
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    chargeCableButton.setChecked(chargeCableIsConnected);
+                    chargeCableButton.setVisibility(View.VISIBLE);
+                }
+            });
+        }else{
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    chargeCableButton.setChecked(chargeCableIsConnected);
+                    chargeCableButton.setVisibility(View.INVISIBLE);
+                    findViewById(R.id.connect_sim_button).setEnabled(false);
+                }
+            });
+        }
+        timer = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Zeppelin", "Loop yau run");
+                MainActivity.this.imageCaptureRequested = true;
+                handler.postDelayed(this, 2000);
+            }
+        };
+        handler.post(timer);
+    }
+
+    /**
+     * Indicate to the user that the device has disconnected
+     */
+    public void onDeviceDisconnected(Device device){
+        Log.i("ExampleApp", "Device disconnected!");
+
+        final ToggleButton chargeCableButton = (ToggleButton)findViewById(R.id.chargeCableToggle);
+        final TextView levelTextView = (TextView)findViewById(R.id.batteryLevelTextView);
+        final ImageView chargingIndicator = (ImageView)findViewById(R.id.batteryChargeIndicator);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                thermalImageView.setImageBitmap(Bitmap.createBitmap(1, 1, Bitmap.Config.ALPHA_8));
+                levelTextView.setText("--");
+                chargeCableButton.setChecked(chargeCableIsConnected);
+                chargeCableButton.setVisibility(View.INVISIBLE);
+                chargingIndicator.setVisibility(View.GONE);
+                thermalImageView.clearColorFilter();
+                findViewById(R.id.tuningProgressBar).setVisibility(View.GONE);
+                findViewById(R.id.tuningTextView).setVisibility(View.GONE);
+                findViewById(R.id.connect_sim_button).setEnabled(true);
+            }
+        });
+        handler.removeCallbacks(timer);
+        flirOneDevice = null;
+    }
+
+    /**
+     * If using RenderedImage.ImageType.ThermalRadiometricKelvinImage, you should not rely on
+     * the accuracy if tuningState is not Device.TuningState.Tuned
+     * @param tuningState
+     */
+    public void onTuningStateChanged(Device.TuningState tuningState){
+        Log.i("ExampleApp", "Tuning state changed changed!");
+
+        currentTuningState = tuningState;
+        if (tuningState == Device.TuningState.InProgress){
+            runOnUiThread(new Thread(){
+                @Override
+                public void run() {
+                    super.run();
+                    thermalImageView.setColorFilter(Color.DKGRAY, PorterDuff.Mode.DARKEN);
+                    findViewById(R.id.tuningProgressBar).setVisibility(View.VISIBLE);
+                    findViewById(R.id.tuningTextView).setVisibility(View.VISIBLE);
+                }
+            });
+        }else {
+            runOnUiThread(new Thread() {
+                @Override
+                public void run() {
+                    super.run();
+                    thermalImageView.clearColorFilter();
+                    findViewById(R.id.tuningProgressBar).setVisibility(View.GONE);
+                    findViewById(R.id.tuningTextView).setVisibility(View.GONE);
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onAutomaticTuningChanged(boolean deviceWillTuneAutomatically) {
+
+    }
+    private ColorFilter originalChargingIndicatorColor = null;
+    @Override
+    public void onBatteryChargingStateReceived(final Device.BatteryChargingState batteryChargingState) {
+        Log.i("ExampleApp", "Battery charging state received!");
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ImageView chargingIndicator = (ImageView) findViewById(R.id.batteryChargeIndicator);
+                if (originalChargingIndicatorColor == null) {
+                    originalChargingIndicatorColor = chargingIndicator.getColorFilter();
+                }
+                switch (batteryChargingState) {
+                    case FAULT:
+                    case FAULT_HEAT:
+                        chargingIndicator.setColorFilter(Color.RED);
+                        chargingIndicator.setVisibility(View.VISIBLE);
+                        break;
+                    case FAULT_BAD_CHARGER:
+                        chargingIndicator.setColorFilter(Color.DKGRAY);
+                        chargingIndicator.setVisibility(View.VISIBLE);
+                    case MANAGED_CHARGING:
+                        chargingIndicator.setColorFilter(originalChargingIndicatorColor);
+                        chargingIndicator.setVisibility(View.VISIBLE);
+                        break;
+                    case NO_CHARGING:
+                    default:
+                        chargingIndicator.setVisibility(View.GONE);
+                        break;
+                }
+            }
+        });
+    }
+    @Override
+    public void onBatteryPercentageReceived(final byte percentage){
+        Log.i("ExampleApp", "Battery percentage received!");
+
+        final TextView levelTextView = (TextView)findViewById(R.id.batteryLevelTextView);
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                levelTextView.setText(String.valueOf((int) percentage) + "%");
+            }
+        });
+
+
+    }
+
+    private void updateThermalImageView(final Bitmap frame){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                thermalImageView.setImageBitmap(frame);
+            }
+        });
+    }
+
+    // StreamDelegate method
+    public void onFrameReceived(Frame frame){
+        Log.v("ExampleApp", "Frame received!");
+
+        if (currentTuningState != Device.TuningState.InProgress){
+            frameProcessor.processFrame(frame);
+        }
+    }
+
+    private Bitmap thermalBitmap = null;
+    private ArrayList<Integer> convertPixelData(Bitmap b) {
+        int[] pixels = new int[b.getWidth() * b.getHeight()];
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        b.getPixels(pixels,0,b.getWidth(),0,0,b.getWidth(),b.getHeight());
+        for (int i = 0; i < pixels.length; i++) {
+            result.add(pixels[i] & 0x000000FF);
+        }
+        return result;
+    }
+
+    // Frame Processor Delegate method, will be called each time a rendered frame is produced
+    public void onFrameProcessed(final RenderedImage renderedImage){
+        thermalBitmap = renderedImage.getBitmap();
+        updateThermalImageView(thermalBitmap);
+
+        if (this.imageCaptureRequested) {
+            Log.i("Zeppelin", "Payload sent");
+            //HARD CODE
+            Payload pl = new Payload(22.282464, 114.190497, 400.0, convertPixelData(thermalBitmap));
+
+            Firebase myFirebaseRef = new Firebase("https://scorching-torch-3982.firebaseio.com/frames");
+            myFirebaseRef.push().setValue(pl);
+            this.imageCaptureRequested = false;
+        }
+
+
+    }
+    public void onCaptureImageClicked(View v){
+        // if nothing's connected, let's load an image instead?
+
+        if(flirOneDevice == null && lastSavedPath != null) {
+            // load!
+            File file = new File(lastSavedPath);
+            LoadedFrame frame = new LoadedFrame(file);
+            // load the frame
+            onFrameReceived(frame);
+        } else {
+            this.imageCaptureRequested = true;
+        }
+    }
+
+    public void onConnectSimClicked(View v){
+        if(flirOneDevice == null){
+            try {
+                flirOneDevice = new SimulatedDevice(this, getResources().openRawResource(R.raw.sampleframes), 10);
+                flirOneDevice.setPowerUpdateDelegate(this);
+                chargeCableIsConnected = true;
+            } catch(Exception ex) {
+                flirOneDevice = null;
+                Log.w("FLIROneExampleApp", "IO EXCEPTION");
+                ex.printStackTrace();
+            }
+        }else if(flirOneDevice instanceof SimulatedDevice) {
+            flirOneDevice.close();
+            flirOneDevice = null;
+        }
+    }
+
+    public void onSimulatedChargeCableToggleClicked(View v){
+        if(flirOneDevice instanceof SimulatedDevice){
+            chargeCableIsConnected = !chargeCableIsConnected;
+            ((SimulatedDevice)flirOneDevice).setChargeCableState(chargeCableIsConnected);
+        }
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        thermalImageView = (ImageView) findViewById(R.id.imageView);
+        try {
+            Device.startDiscovery(this, this);
+        }catch(IllegalStateException e){
+            // it's okay if we've already started discovery
+        }
+        mGoogleApiClient.connect();
+    }
+
+    ScaleGestureDetector mScaleDetector;
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+
+        final View controlsView = findViewById(R.id.fullscreen_content_controls);
+        final View controlsViewTop = findViewById(R.id.fullscreen_content_controls_top);
+        final View contentView = findViewById(R.id.fullscreen_content);
+
+        tvInfo = (TextView)findViewById(R.id.tvInfo);
+        buildGoogleApiClient();
+        createLocationRequest();
+
+        Firebase.setAndroidContext(this);
+        handler = new Handler();
+
+        RenderedImage.ImageType defaultImageType = RenderedImage.ImageType.ThermalLinearFlux14BitImage;
+        frameProcessor = new FrameProcessor(this, this, EnumSet.of(defaultImageType));
+        frameProcessor.setImagePalette(RenderedImage.Palette.Gray);
+
+        mScaleDetector = new ScaleGestureDetector(this, new ScaleGestureDetector.OnScaleGestureListener() {
+            @Override
+            public void onScaleEnd(ScaleGestureDetector detector) {
+            }
+            @Override
+            public boolean onScaleBegin(ScaleGestureDetector detector) {
+                return true;
+            }
+            @Override
+            public boolean onScale(ScaleGestureDetector detector) {
+                Log.d("ZOOM", "zoom ongoing, scale: " + detector.getScaleFactor());
+                frameProcessor.setMSXDistance(detector.getScaleFactor());
+                return false;
+            }
+        });
+
+        findViewById(R.id.fullscreen_content).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mScaleDetector.onTouchEvent(event);
+                return true;
+            }
+        });
+
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        if (mGoogleApiClient.isConnected()) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(
+                    mGoogleApiClient, this);
+        }
+    }
+
+
+    @Override
+    public void onRestart(){
+        try {
+            Device.startDiscovery(this, this);
+        } catch (IllegalStateException e) {
+            Log.e("PreviewActivity", "Somehow we've started discovery twice");
+            e.printStackTrace();
+        }
+        super.onRestart();
+    }
+
+    @Override
+    public void onStop() {
+        // We must unregister our usb receiver, otherwise we will steal events from other apps
+        Log.e("PreviewActivity", "onStop, stopping discovery!");
+        Device.stopDiscovery();
+        super.onStop();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.i("Zeppelin", "Succeed in connecting Google API");
+        mCurrentLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mCurrentLocation != null) {
+            updateUI();
+        }
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    private void updateUI() {
+        String str = "Lat: ";
+        str = str + String.valueOf(mCurrentLocation.getLatitude());
+        str = str + " Lng: ";
+        str = str + String.valueOf(mCurrentLocation.getLongitude());
+        str = str + " updatedAt: " + mLastUpdateTime;
+        str = str + " alt: " + String.valueOf(mCurrentLocation.getAltitude());
+        str = str + mCurrentLocation.getProvider();
+        tvInfo.setText(str);
+    }
+
+
+    protected void startLocationUpdates() {
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e("Connection", "Failed " + connectionResult.getErrorCode() + " " + connectionResult.toString());
+
+    }
+
+    /**
+     * Stores activity data in the Bundle.
+     */
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+
+
 }
---
+++ app/src/main/java/com/clionelabs/flirhackathon/Payload.java
@@ -0,0 +1,21 @@
+package com.clionelabs.flirhackathon;
+
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Payload {
+    public final Double latitude;
+    public final Double longitude;
+    public final Double altitude;
+    public final Integer width = 40;
+    public final Integer height = 30;
+    public final List heat;
+    public Payload(Double latitude, Double longitude, Double altitudeInM, ArrayList<Integer> imageGray) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.altitude = altitudeInM;
+        this.heat = imageGray;
+    }
+}
